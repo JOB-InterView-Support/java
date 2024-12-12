ba912924b57b1586351a43632d3f60b9df4570ae
Choi Man Kyeong <choimatthew0326@gmail.com>
2024-12-12 13:05:06 +0900

    test

--- jobis/src/main/java/org/myweb/jobis/JobisApplication.java
+++ jobis/src/main/java/org/myweb/jobis/JobisApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobisApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JobisApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JobisApplication.class, args);
+	}
 
 }
