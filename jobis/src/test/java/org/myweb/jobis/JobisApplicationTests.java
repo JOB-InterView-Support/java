28831ca2583371d59f068940d4edcbd5b72be33a
Choi Man Kyeong <choimatthew0326@gmail.com>
2024-12-12 14:07:33 +0900

    test

---
+++ jobis/src/test/java/org/myweb/jobis/JobisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myweb.jobis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
