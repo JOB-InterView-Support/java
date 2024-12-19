bcd2ab2197571049a5472c7b0f810d9cb2adc222
choiwj0706 <choiwj0706@gmail.com>
2024-12-19 17:46:32 +0900

    Add MyPage getUserInfo

--- jobis/src/main/java/org/myweb/jobis/mypage/controller/MypageController.java
+++ jobis/src/main/java/org/myweb/jobis/mypage/controller/MypageController.java
@@ -3,16 +3,31 @@ package org.myweb.jobis.mypage.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.myweb.jobis.mypage.model.service.MypageService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.myweb.jobis.user.jpa.entity.UserEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/mypage")
 @RequiredArgsConstructor
 @CrossOrigin
 public class MypageController {
     private final MypageService mypageService;
 
+    // 회원 정보 불러오기
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserEntity> getUserInfo(@PathVariable String userId) {
+        UserEntity userInfo = mypageService.getUserInfo(userId);
+        return ResponseEntity.ok(userInfo);
+    }
+
+    // 회원 정보 수정
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserEntity> updateUser(
+            @PathVariable String userId,
+            @RequestBody UserEntity updatedUser) {
+        UserEntity result = mypageService.updateUser(userId, updatedUser.getUserName(), updatedUser.getUserPw(), updatedUser.getUserPhone(), updatedUser.getUserDefaultEmail());
+        return ResponseEntity.ok(result);
+    }
 }
