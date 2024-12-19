3fce87f3fd12b80b4f5d085ec2396dae92b59b41
Sonyj8658 <sda8658@gmail.com>
2024-12-19 18:47:41 +0900

    jobPosting DTO ENTITY 수정

--- jobis/src/main/java/org/myweb/jobis/jobposting/controller/JobPostingController.java
+++ jobis/src/main/java/org/myweb/jobis/jobposting/controller/JobPostingController.java
@@ -1,4 +1,12 @@
 package org.myweb.jobis.jobposting.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+@Slf4j
+@RestController
+@RequestMapping("/api/job-postings")
 public class JobPostingController {
+
 }
