1e6d2cad58c79e26c722438cce79d3ffc6634561
choiwj0706 <choiwj0706@gmail.com>
2024-12-26 17:42:46 +0900

    Add select Myintro / detailview

--- jobis/src/main/java/org/myweb/jobis/mypage/jpa/repository/SelfIntroduceRepository.java
+++ jobis/src/main/java/org/myweb/jobis/mypage/jpa/repository/SelfIntroduceRepository.java
@@ -1,13 +1,17 @@
 package org.myweb.jobis.mypage.jpa.repository;
 
 import org.myweb.jobis.mypage.jpa.entity.SelfIntroduceEntity;
-import org.myweb.jobis.mypage.model.dto.SelfIntroduce;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SelfIntroduceRepository extends JpaRepository<SelfIntroduceEntity, String> {
     List<SelfIntroduceEntity> findByUuid(String uuid);
+
+    Optional<SelfIntroduceEntity> findByIntroNo(String introNo);
 }
+
+
