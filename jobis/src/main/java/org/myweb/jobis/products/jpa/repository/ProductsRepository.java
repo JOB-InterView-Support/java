ffed129b0976e3eb4f923c1ae2cefed270e4fc4b
choiwj0706 <choiwj0706@gmail.com>
2025-01-02 15:35:10 +0900

    이용권 내역 관련 컬럼 추가 및 수정 / 사용 횟수 미차감시 환불 가능하도록 하는 로직 추가

--- jobis/src/main/java/org/myweb/jobis/products/jpa/repository/ProductsRepository.java
+++ jobis/src/main/java/org/myweb/jobis/products/jpa/repository/ProductsRepository.java
@@ -4,6 +4,8 @@ import org.myweb.jobis.products.jpa.entity.ProductsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ProductsRepository extends JpaRepository<ProductsEntity, Integer>{
+
 }
